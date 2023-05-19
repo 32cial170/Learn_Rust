50c9aafe1f6ed4abb9fc1e4cccf754943857db22
roger <rs29636383@gmail.com>
2023-05-19 10:01:48 +0800

    ownership

---
+++ ownerShip/target/debug/deps/ownerShip-2785f9d79cf57aeb.d
@@ -0,0 +1,5 @@
+/home/ubuntu/rust/ownerShip/target/debug/deps/ownerShip-2785f9d79cf57aeb.rmeta: src/main.rs
+
+/home/ubuntu/rust/ownerShip/target/debug/deps/ownerShip-2785f9d79cf57aeb.d: src/main.rs
+
+src/main.rs:
---
+++ ownerShip/target/debug/deps/ownerShip-7b3fdd86900e10d3.d
@@ -0,0 +1,5 @@
+/home/ubuntu/rust/ownerShip/target/debug/deps/ownerShip-7b3fdd86900e10d3.rmeta: src/main.rs
+
+/home/ubuntu/rust/ownerShip/target/debug/deps/ownerShip-7b3fdd86900e10d3.d: src/main.rs
+
+src/main.rs:
